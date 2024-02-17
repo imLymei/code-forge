d6fca201508add101e0e3412d9e5247c991776ad
imLymei <lymeicontato@gmail.com>
2024-02-17 18:26:38 -0300

    feat: Added action to publish code to NPM

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 const cliConfig = {
 	name: 'Code Forge',
-	version: '1.0.0',
+	version: '1.1.0',
 	description:
 		'Code Forge is a command-line tool for effortlessly creating new projects in any language. Customize commands in a config file to streamline React, Next.js, Rust, Python, and more, all within your preferred directories. Simplify project setup and accelerate development with Code Forge.',
 };
